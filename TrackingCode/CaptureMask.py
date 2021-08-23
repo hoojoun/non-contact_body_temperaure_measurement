4fd621da6a866dfb472d0e0321df9927a83ab00b
charyuza <charyuza98@naver.com>
2021-08-23 11:06:12 +0900

    first sh commit

---
+++ TrackingCode/CaptureMask.py
@@ -0,0 +1,82 @@
+# 얼굴 이미지를 입력받아 마스크 착용 여부 검출, 촬영하는 프로그램
+# 10프레임마다 촬영함
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+
+# readNet으로 모델 읽기 -> 하나의 Net 객체 반환
+facenet = cv2.dnn.readNet('models/deploy.prototxt', 'models/res10_300x300_ssd_iter_140000.caffemodel')
+model = load_model('models/mask_detector.model')
+
+cam = cv2.VideoCapture(0)
+
+if not cam.isOpened():
+    print("카메라를 열 수 없습니다.")
+    exit()
+
+cam_cnt = 0 # 프레임 가산
+pic_num = 0 # 사진 이름
+
+while True:
+    ret, img = cam.read()
+
+    if ret == False:    
+        break
+
+    h, w, c = img.shape
+    # 이미지 전처리하기
+    # blobFromImage; 이미지를 가지고 4차원의 blob 만들기, resize 포함
+    blob = cv2.dnn.blobFromImage(img, size=(300, 300), mean=(104., 177., 123.))
+
+    # 얼굴 영역 탐지 모델로 추론하기
+    # blob를 신경망에 추가, setInput으로 입력받기
+    # forward() 통해 신경망이 학습데이터 입력받은 후 forward propagation 진행
+    facenet.setInput(blob)
+    dets = facenet.forward()
+
+    # 각 얼굴에 대해서 반복문 돌기
+    for i in range(dets.shape[2]):
+        confidence = dets[0, 0, i, 2]
+
+        if confidence < 0.5:    # min_confidence
+            continue
+
+        # 사각형 꼭지점 찾기; x1 = int(dets[0, 0, i, 3] * w) 형식도 무방함
+        box = dets[0,0,i,3:7]*np.array([w, h, w, h])
+        (x1,y1,x2,y2) = box.astype('int')
+
+        face = img[y1:y2, x1:x2]
+
+        # <전처리>
+        # resizing, BGR->RGB, 전처리 연산, 차원 변형
+        face_input = cv2.resize(face, dsize=(224, 224))
+        face_input = cv2.cvtColor(face_input, cv2.COLOR_BGR2RGB)
+        face_input = preprocess_input(face_input)
+        face_input = np.expand_dims(face_input, axis=0)
+
+        mask, nomask = model.predict(face_input).squeeze()
+        cam_cnt += 1
+
+        if mask > nomask:
+            if cam_cnt % 10 == 0:
+                pic_num += 1
+                face_slice = img[y1:y2, x1:x2, :]
+                cv2.imwrite('./TestDetector/mask/mask'+str(pic_num)+'.jpg', face_slice)
+            
+            color = (0, 255, 0)
+            cv2.putText(img, text='%.2f'%mask, org=(x1, y1), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=color)
+        else:
+            if cam_cnt % 10 == 0:
+                pic_num += 1
+                face_slice = img[y1:y2, x1:x2, :]
+                cv2.imwrite('./TestDetector/nomask/nomask'+str(pic_num)+'.jpg', face_slice)
+            color = (0, 0, 255)
+            cv2.putText(img, text='%.2f'%mask, org=(x1, y1), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=color)
+            
+        # 사각형 그리기
+        cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), thickness=2, color=color)
+
+    cv2.imshow('result', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
---
+++ TrackingCode/landmark_5point.py
@@ -0,0 +1,55 @@
+# five point model with dlib
+# 마스크를 쓴 상태에선 인식 안됨
+# 분기 만들어서 재사용할지 현재 고민중
+
+import cv2
+import dlib
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('models/shape_predictor_5_face_landmarks.dat')
+
+cap = cv2.VideoCapture(0) # 'imgs/01.mp4'
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+# print('original size: %d, %d' % (width, height))
+
+while cap.isOpened():
+    ret, img = cap.read()
+
+    if ret:
+        faces = detector(img)
+        for face in faces:  # 얼굴 감지
+            shape = predictor(img, face) # 랜드마크 저장 (전체 이미지 프레임, 얼굴 영역 좌표)
+            
+            # 점의 순서를 알아내기 위함; enumerate를 for문에서 사용할 경우 순서와 값을 동시에 출력함
+            for i in shape.parts():
+                cv2.circle(img, center=(i.x, i.y), radius=2, color=(0, 0, 255), thickness=-1)   # 점 그리기, BGR
+                y_start = (shape.part(3).y+shape.part(1).y)/2-(shape.part(0).x-shape.part(1).x-shape.part(2).x+shape.part(3).x)/2   #3.x~1.x - 평균 눈크기
+                y_end = (shape.part(3).y+shape.part(1).y)/2+(shape.part(0).x-shape.part(1).x-shape.part(2).x+shape.part(3).x)/2     #3.x~1.x + 평균 눈크기
+                cv2.rectangle(img, (shape.part(3).x,int(y_start)), (shape.part(1).x,int(y_end)), color=(255, 255, 255), thickness=1)
+                roi_center = ((shape.part(3).x+shape.part(1).x)/2,(shape.part(3).y+shape.part(1).y)/2)
+                cap_center = (width/2, height/2)
+                # print(shape.part(1), shape.part(1).x, shape.part(1).y, roi_center, cap_center)
+                if roi_center[1]<cap_center[1]:
+                    print('move down')
+                elif roi_center[1]>cap_center[1]:
+                    print('move up')
+                if roi_center[0]<cap_center[0]:
+                    print('move right')
+                elif roi_center[0]>cap_center[0]:
+                    print('move left')
+                
+            try:
+                x1 = face.left()
+                y1 = face.top()
+                x2 = face.right()
+                y2 = face.bottom()
+
+                cv2.rectangle(img, pt1=(x1, y1), pt2=(x2, y2), color=(255, 0, 0), thickness=2)
+
+            except:
+                pass
+        cv2.imshow('result', img)
+        
+    if cv2.waitKey(1) == ord('q'):
+        break
